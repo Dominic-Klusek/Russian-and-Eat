e2fb6735bb0b5f84d13ab1d66de5c8a0426acb95
Goldensonic96 <Goldensonic96@yahoo.com>
2017-10-05 17:47:51 -0400

    V1.1
    Fixed movement, still no sprites

--- Assets/Move.cs
+++ Assets/Move.cs
@@ -19,42 +19,49 @@ public class Move : MonoBehaviour {
 		
 	}
 
+	//called on when click on collider
 	void OnMouseDown()
 	{
 		bool leftClick = Input.GetMouseButtonDown (0);
 		if (leftClick) {
 			Vector3 v = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			v = new Vector3 (Mathf.Floor (v.x), Mathf.Floor (v.y), Mathf.Floor (v.z));
+			StartCoroutine(MoveX (v));
 
-			MoveX (v);
-
-			MoveY (v);
+			StartCoroutine(MoveY (v));
 
 		}
 	}
 
-	void MoveX(Vector3 v)
+	IEnumerator MoveX(Vector3 v)
 	{
 		//while loop doesn't work, need to figure out why
 		//while(positionCharacter.position.x != v.x)
 		//while the clicked position doesn't equal the object's position stay in loop
+		while (positionCharacter.position.x != v.x) {
 			if (positionCharacter.position.x > v.x) {
-				bodyCharacter.MovePosition (new Vector2 (--positionCharacter.position.x, transform.position.y));
+				bodyCharacter.MovePosition (new Vector2 (--positionCharacter.position.x, positionCharacter.position.y));
 			}
 			else if (positionCharacter.position.x < v.x) {
-				bodyCharacter.MovePosition (new Vector2 (++positionCharacter.position.x, transform.position.y));
+				bodyCharacter.MovePosition (new Vector2 (++positionCharacter.position.x, positionCharacter.position.y));
 			}
-
+			yield return null;
+		}
+			
 	}
 
-	void MoveY(Vector3 v)
+	IEnumerator MoveY(Vector3 v)
 	{
 		//while the clicked position doesn't equal the object's position stay in loop
-		if (positionCharacter.position.y > v.y) {
-			bodyCharacter.MovePosition (new Vector2 (positionCharacter.position.x, --transform.position.y));
+		while (positionCharacter.position.y != v.y) {
+			if (positionCharacter.position.y > v.y) {
+				bodyCharacter.MovePosition (new Vector2 (positionCharacter.position.x, --positionCharacter.position.y));
+			}
+			else if (positionCharacter.position.y < v.y) {
+				bodyCharacter.MovePosition (new Vector2 (positionCharacter.position.x, ++positionCharacter.position.y));
+			}
+			yield return null;
 		}
-		else if (positionCharacter.position.y < v.y) {
-			bodyCharacter.MovePosition (new Vector2 (positionCharacter.position.x, ++transform.position.y));
-		}
+
 	}
 }
