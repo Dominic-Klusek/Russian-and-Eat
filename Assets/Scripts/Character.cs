5854e6c3dcda7f4bbfd1f9a60e7f583fa31652c0
briankeegan1 <briankeeganmusic@gmail.com>
2017-11-09 15:21:47 -0500

    Deleted extra game manager object that caused an error and other small fixes.

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour {
-	public GameManager gm;
 	public bool finishedMovement = true;
 
 	// Use this for initialization
