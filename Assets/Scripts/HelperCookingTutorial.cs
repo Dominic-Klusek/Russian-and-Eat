3216103f3f780fea8cd40f08960a7f9df83cd566
Ethan <ethanbinya@aol.com>
2017-11-23 15:45:25 -0500

    Continuing making the tutorial.

--- Assets/Scripts/HelperCookingTutorial.cs
+++ Assets/Scripts/HelperCookingTutorial.cs
@@ -4,90 +4,57 @@ using UnityEngine.UI;
 using UnityEngine;
 
 public class HelperCookingTutorial : MonoBehaviour {
-    
-    public int textSpot = 0;
-	Text dialogue;
-	public Texture btnTexture;
+    private int tutorialStage = 0;
+    private Text dialogue;
+    private Character player;
+
+    public Texture btnTexture;
 	public GameObject maleObject;
 	public GameObject femaleObject;
 
 	// Use this for initialization
 	void Start () {
 		dialogue = GetComponent<Text> ();
-        
+        player = Character.getInstance();
 }
 	
 	// Update is called once per frame
 	void Update () {
-        //if (GameObject.FindBB)
-	}
+        if (FridgeUI.getInstance() != null && tutorialStage == 0)
+            tutorialStage = 1;
+        if (player.getCharacterDish().getIngredientArray().Length == 2 && tutorialStage == 1)
+            tutorialStage = 2;
+    }
 
-	void OnGUI(){
+    void OnGUI(){
 		GameManager game = Object.FindObjectOfType<GameManager>();
-		switch (textSpot) {
-		case 0:
-			dialogue.text = "Since you're new here, I'll show you around the kitchen.\n" +
-				"Clicking on a floor tile will move you to that space.\n" +
-                "Clicking on the fridge will move you to it and open it up." + 
-                "Try it now.";
+		switch (tutorialStage) {
+		    case 0:
+                dialogue.text = "Since you're new here, I'll show you around the kitchen.\n" +
+                    "Clicking on the floor will move you to that space.\n" +
+                    "Clicking on the fridge will move you to it and open it up.\n" +
+                    "Try seeing what we have in the fridge.";
 			break;
-		case 1:
-			displayCharacters ();
-			dialogue.text = "Before we get started I need to know,\nwho would you like to play as?";
-			if (GUI.Button (new Rect (375, 300, 60, 60), "Male")) {
-				hideFemaleCharacter ();
-				print ("Male");
-				game.femaleCharacter = false;
-				textSpot++;
-			}
-			if (GUI.Button (new Rect (550, 300, 60, 60), "Female")) {
-				hideMaleCharacter ();
-				print ("Female");
-				game.femaleCharacter = true;
-				textSpot++;
-			}
-			break;
-		case 2:
-			dialogue.text = "Great! Click Play when you're ready to start!";
-			if (GUI.Button (new Rect (750, 400, 50, 50), "PLAY")) {
-				game.LoadLevel1 ();
-			}
-			break;
-		default:
-			break;
+		    case 1:
+                dialogue.text = "Great! Now lets make a dish.\n" +
+                                "If you look to the left, you'll see a ticket.\n" +
+                                "It explains what food was ordered and how to make it.\n" +
+                                "Clicking on an ingredient will add it to your dish.\n";
+                break;
+		    case 2:
+                dialogue.text = "\n" +
+                                "\n" +
+                                "\n" +
+                                "\n";
+                break;
+            case 3:
+                dialogue.text = "\n" +
+                                "\n" +
+                                "\n" +
+                                "\n";
+                break;
+            default:
+			    break;
 		}
 	}
-
-	void displayCharacters(){
-		maleObject = GameObject.Find ("chef_male_0");
-		femaleObject = GameObject.Find ("chef_female_0");
-		maleObject.GetComponent<SpriteRenderer>().enabled = true;
-		femaleObject.GetComponent<SpriteRenderer>().enabled = true;
-	}
-	void hideMaleCharacter(){
-		maleObject.GetComponent<SpriteRenderer>().enabled = false;
-	}
-	void hideFemaleCharacter(){
-		femaleObject.GetComponent<SpriteRenderer>().enabled = false;
-	}
-
-	/*void TextChanger(){
-		switch (textSpot) {
-		case 0:
-			dialogue.text = "Welcome to Russian-and-Eat. I'm your guide, Vladimir.\n" +
-				"The goal of this game is to teach you the Russian language and to cook Russian cuisine.";
-			break;
-		case 1:
-			dialogue.text = "Before we get started I need to know,\nAre you male or female.";
-			if (GUI.Button (new Rect (10, 10, 50, 50), btnTexture)) {
-				
-			}
-			break;
-		case 2:
-			dialogue.text = "Please enter you name: ";
-			break;
-		default:
-			break;
-		}
-	}*/
 }
