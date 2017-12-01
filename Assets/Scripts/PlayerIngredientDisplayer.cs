38202222cc74d981db0964b091c7852f9dc34b36
Ethan <ethanbinya@aol.com>
2017-12-01 00:44:52 -0500

    Added script for ingredient display in status bar. In progress.

---
+++ Assets/Scripts/PlayerIngredientDisplayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerIngredientDisplayer : MonoBehaviour {
+    private Text buttonText;
+    private List<string> ingredientTransliterationsToDisplay;
+    private string mostRecentIngredient;
+
+	// Use this for initialization
+	void Start () {
+		buttonText = GetComponentInChildren<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //buttonText.text = "s";
+    }
+
+    public void updateIngredientsDisplayed(Dish playerDish)
+    {
+        List<Ingredient> dishIngredients = playerDish.getIngredientList();
+        mostRecentIngredient = dishIngredients[dishIngredients.Count - 1].getTransliteration();
+        buttonText.text = mostRecentIngredient;
+    }
+}
