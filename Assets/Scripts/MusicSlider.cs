e3a21b29782a533be2236c288543cedf23bda7ff
Ethan <ethanbinya@aol.com>
2017-11-30 23:34:30 -0500

    Added scripts for music and sound sliders. Changing a slider with one of the script attached will change the volume of the music/sounds.

---
+++ Assets/Scripts/MusicSlider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicSlider : MonoBehaviour {
+    private Slider musicSlider;
+
+    // Use this for initialization
+    void Start () {
+        musicSlider = GetComponentInChildren<Slider>();
+        musicSlider.onValueChanged.AddListener(delegate { changeMusicVolume(); });
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    void changeMusicVolume()
+    {
+        SoundManager.getInstance().setMusicVolume(musicSlider.value);
+    }
+}
---
+++ Assets/Scripts/SoundSlider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundSlider : MonoBehaviour {
+    private Slider soundSlider;
+
+	// Use this for initialization
+	void Start () {
+        soundSlider = GetComponentInChildren<Slider>();
+        soundSlider.onValueChanged.AddListener(delegate { changeMusicVolume(); });
+    }
+
+    // Update is called once per frame
+    void Update () {
+	}
+
+    void changeMusicVolume()
+    {
+        SoundManager.getInstance().setSoundVolume(soundSlider.value);
+    }
+}
