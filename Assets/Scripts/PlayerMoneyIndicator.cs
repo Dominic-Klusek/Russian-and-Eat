d57ae5a60cfd071ebe5676e319baa5c62d6e1b99
Ethan <ethanbinya@aol.com>
2017-12-01 01:25:03 -0500

    Added script to display player's money.

---
+++ Assets/Scripts/PlayerMoneyIndicator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMoneyIndicator : MonoBehaviour {
+    private Text buttonText;
+    private int playerMoney;
+
+    // Use this for initialization
+    void Start () {
+        buttonText = GetComponentInChildren<Text>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void updatePlayerMoneyDisplayed()
+    {
+        buttonText.text = GameManager.getInstance().getPlayerMoney().ToString();
+    }
+
+}
