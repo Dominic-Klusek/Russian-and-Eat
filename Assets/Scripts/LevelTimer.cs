cd944172534859bddd400d2619e2ca8a83bf8cca
Ethan <ethanbinya@aol.com>
2017-11-30 22:14:56 -0500

    Deleted Status Bar prefab and instead made a prefab from the canvas the status bar uses. Added script to change timer bar's value over time to fill out in the given time (default is 60 seconds), change timer bar's fill color to go from green to red as it fills out, and made the timer bar load the next scene when filled out.

---
+++ Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelTimer : MonoBehaviour {
+    public int secondsPerRound = 60;
+    public bool timerActive = true;
+
+    private int currentTime = 0;
+    private int timeIncrement = 0;
+    private Slider timer;
+    private Image timerFill;
+
+	// Use this for initialization
+	void Start () {
+        //timer = FindObjectOfType<Slider>();
+        timer = GameObject.Find("Time Bar").GetComponent<Slider>();
+        //timerFill = timer.GetComponentInChildren<Image>();
+        timerFill = timer.transform.Find("Fill Area").GetComponentInChildren<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timerActive)
+        {
+            timer.value += Time.deltaTime / secondsPerRound;
+            timerFill.color = Color.Lerp(Color.green, Color.red, timer.value);
+            if (timer.value >= 1)
+                GameManager.getInstance().LoadNextScene();
+        }
+    }
+}
