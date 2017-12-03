bd47a770aa32b72af9aa4a552a54bc8f5e6a30b6
Ethan <ethanbinya@aol.com>
2017-12-02 21:15:01 -0500

    Added public float and code to hide the slider when it hasnt been interacted with. Default number of seconds until hiding is 3.

--- Assets/Scripts/MusicSlider.cs
+++ Assets/Scripts/MusicSlider.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class MusicSlider : MonoBehaviour {
+    public float secondsUntilHidingIdleSlider = 3;
+
     private Slider musicSlider;
     private Button musicButton;
     private Animator sliderAnimator;
+    private bool isSliderHidden;
+    private float secondsSliderIdle = 0;
+    
     // Use this for initialization
     void Start () {
         musicSlider = GetComponentInChildren<Slider>();
@@ -21,17 +26,26 @@ public class MusicSlider : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-		
-	}
+        if (!isSliderHidden)
+        {
+            secondsSliderIdle += Time.deltaTime;
+            if (secondsSliderIdle >= secondsUntilHidingIdleSlider)
+            {
+                toggleSlider();
+                secondsSliderIdle = 0;
+            }
+        }
+    }
 
     void changeMusicVolume()
     {
         SoundManager.getInstance().setMusicVolume(musicSlider.value);
+        secondsSliderIdle = 0;
     }
 
     private void toggleSlider()
     {
-        // toggle isHidden bool
-        sliderAnimator.SetBool("isHidden", !sliderAnimator.GetBool("isHidden"));
+        isSliderHidden = !isSliderHidden;
+        sliderAnimator.SetBool("isHidden", isSliderHidden);
     }
 }
--- Assets/Scripts/SoundSlider.cs
+++ Assets/Scripts/SoundSlider.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class SoundSlider : MonoBehaviour {
+    public float secondsUntilHidingIdleSlider = 3;
+
     private Slider soundSlider;
     private Button soundButton;
     private Animator sliderAnimator;
+    private bool isSliderHidden;
+    private float secondsSliderIdle = 0;
 
     // Use this for initialization
     void Start () {
@@ -18,19 +22,30 @@ public class SoundSlider : MonoBehaviour {
         soundButton.onClick.AddListener(delegate { toggleSlider(); });
 
         sliderAnimator = soundSlider.GetComponent<Animator>();
+        isSliderHidden = sliderAnimator.GetBool("isHidden");
     }
 
     // Update is called once per frame
     void Update () {
+        if (!isSliderHidden) {
+            secondsSliderIdle += Time.deltaTime;
+            if (secondsSliderIdle >= secondsUntilHidingIdleSlider)
+            {
+                toggleSlider();
+                secondsSliderIdle = 0;
+            }
+        }
 	}
 
     void changeSoundVolume()
     {
         SoundManager.getInstance().setSoundVolume(soundSlider.value);
+        secondsSliderIdle = 0;
     }
 
     private void toggleSlider()
     {
-        sliderAnimator.SetBool("isHidden", !sliderAnimator.GetBool("isHidden"));
+        isSliderHidden = !isSliderHidden;
+        sliderAnimator.SetBool("isHidden", isSliderHidden);
     }
 }
