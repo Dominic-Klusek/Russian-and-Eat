47ba2ea5598c76014f88dec632cf140f13a20bd7
Ethan <ethanbinya@aol.com>
2017-12-02 20:52:30 -0500

    Added animations to music and sound button sliders. Clicking either button will show its respective slider.

--- Assets/Scripts/MusicSlider.cs
+++ Assets/Scripts/MusicSlider.cs
@@ -5,12 +5,18 @@ using UnityEngine.UI;
 
 public class MusicSlider : MonoBehaviour {
     private Slider musicSlider;
-
+    private Button musicButton;
+    private Animator sliderAnimator;
     // Use this for initialization
     void Start () {
         musicSlider = GetComponentInChildren<Slider>();
         musicSlider.value = SoundManager.getInstance().getMusicVolume();
         musicSlider.onValueChanged.AddListener(delegate { changeMusicVolume(); });
+
+        musicButton = musicSlider.GetComponentInParent<Button>();
+        musicButton.onClick.AddListener(delegate { toggleSlider(); });
+
+        sliderAnimator = musicSlider.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,5 +27,11 @@ public class MusicSlider : MonoBehaviour {
     void changeMusicVolume()
     {
         SoundManager.getInstance().setMusicVolume(musicSlider.value);
+    }
+
+    private void toggleSlider()
+    {
+        // toggle isHidden bool
+        sliderAnimator.SetBool("isHidden", !sliderAnimator.GetBool("isHidden"));
     }
 }
--- Assets/Scripts/SoundSlider.cs
+++ Assets/Scripts/SoundSlider.cs
@@ -5,12 +5,19 @@ using UnityEngine.UI;
 
 public class SoundSlider : MonoBehaviour {
     private Slider soundSlider;
+    private Button soundButton;
+    private Animator sliderAnimator;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         soundSlider = GetComponentInChildren<Slider>();
         soundSlider.value = SoundManager.getInstance().getSoundVolume();
         soundSlider.onValueChanged.AddListener(delegate { changeSoundVolume(); });
+
+        soundButton = soundSlider.GetComponentInParent<Button>();
+        soundButton.onClick.AddListener(delegate { toggleSlider(); });
+
+        sliderAnimator = soundSlider.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,4 +29,8 @@ public class SoundSlider : MonoBehaviour {
         SoundManager.getInstance().setSoundVolume(soundSlider.value);
     }
 
+    private void toggleSlider()
+    {
+        sliderAnimator.SetBool("isHidden", !sliderAnimator.GetBool("isHidden"));
+    }
 }
