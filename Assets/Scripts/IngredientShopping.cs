f13a72b505abf14cd0f868e2d384f9a8c325f57a
Ethan <ethanbinya@aol.com>
2017-12-03 02:23:47 -0500

    Added shopping scene. Plays after timer runs out in game. Has button to take back to game. Can buy ingredients. Clicking an ingredient button will make it disappear and add it to the ingredientsAvailable list in GM, but other buttons won't be placed in correct place (there will simply be a gap where that clicked button was). In progress.

---
+++ Assets/Scripts/IngredientShopping.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IngredientShopping : MonoBehaviour {
+    public int costPerIngredient = 100;
+    public int buttonSpacing = 50;
+    public GameObject buyItemButtonPrefab;
+
+    private List<Ingredient> unPurchasedIngredientList;
+    private List<Button> ingredientBuyingButtons;
+
+    // Use this for initialization
+    void Start()
+    {
+        ingredientBuyingButtons = new List<Button>();
+        List<Ingredient> allIngredientList = GameManager.getInstance().getAllIngredients();
+        List<Ingredient> purchasedIngredientList = GameManager.getInstance().getIngredientsAvailable();
+        unPurchasedIngredientList = allIngredientList.Except(purchasedIngredientList).ToList();
+
+        var scrollContentContainer = transform.Find("Viewport/Content");
+        //Button button = scrollContentContainer.GetComponentInChildren<Button>();
+        //button.GetComponentInChildren<Text>().text = dishList[1].ToString();
+        for (int i = 0; i < unPurchasedIngredientList.Count; i++)
+        {
+            GameManager.getInstance().awardPlayerMoney(2 * costPerIngredient);
+
+            GameObject button = Instantiate(buyItemButtonPrefab) as GameObject;
+
+            // makes the button a child of the scroll container
+            // false makes its transform local to the new parent
+            button.transform.SetParent(scrollContentContainer.transform, false);
+            button.transform.Translate(0, -buttonSpacing * i, 0);
+
+            Button buttonElement = button.GetComponent<Button>();
+            ingredientBuyingButtons.Add(buttonElement);
+            buttonElement.GetComponentInChildren<Text>().text = unPurchasedIngredientList[i].ToString();
+            // do not change this.
+            // for some reason, passing i makes the method its being sent to get 2 (when tested with 2 ingredients)
+            // throwing out of index exception. giving value of i to a variable
+            // and passing the variable works fine though.
+            int indexForArgument = i;
+            buttonElement.onClick.AddListener(delegate { buyIngredientAndRemoveButton(indexForArgument); });
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void buyIngredientAndRemoveButton(int ingredientIndex)
+    {
+        Debug.Log("index = " + ingredientIndex);
+        if (GameManager.getInstance().getPlayerMoney() > costPerIngredient)
+        {
+            Destroy(ingredientBuyingButtons[ingredientIndex].gameObject);
+            //ingredientBuyingButtons.RemoveAt(ingredientIndex);
+
+            GameManager.getInstance().addIngredientToAvailableIngredientsList(
+                unPurchasedIngredientList[ingredientIndex]);
+            GameManager.getInstance().spendMoney(costPerIngredient);
+        }
+        else
+            StartCoroutine(indicateIngredientNotBought(ingredientBuyingButtons[ingredientIndex]));
+    }
+
+    private IEnumerator indicateIngredientNotBought(Button button)
+    {
+        button.image.color = Color.red;
+        yield return new WaitForSeconds(1);
+        button.image.color = Color.white;
+    }
+}
---
+++ Assets/Scripts/StartNewDay.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartNewDay : MonoBehaviour {
+    public void startNewDay()
+    {
+        GameManager.getInstance().LoadLevel1();
+    }
+}
